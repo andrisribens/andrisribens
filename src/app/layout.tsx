1b3766aaf0e8bf425ac17ba98162efe0c5c1b463
Andris Rībens <andris.ribens@gmail.com>
2025-05-11 19:27:04 +0300

    Add some metadata, more mobile styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
-'use client';
+// 'use client';
 import type { Metadata } from 'next';
 import '../styles/globals.scss';
 import Footer from '../components/footer/Footer';
 
 // Optional: Uncomment and customize metadata if needed
-// export const metadata: Metadata = {
-//   title: 'My App Title',
-//   description: 'App description here',
-// };
+export const metadata: Metadata = {
+  title: 'Andris Rībens website',
+  description:
+    'This is Andris Rībens personal website, where I showcase some of my personal projets.',
+};
 
 export default function RootLayout({
   children,
