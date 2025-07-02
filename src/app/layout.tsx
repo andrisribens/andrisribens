e6eba133c2053d80adb728f9483538fbce3bd520
Andris Rībens <andris.ribens@gmail.com>
2025-07-02 17:45:41 +0300

    Add GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-// 'use client';
+import Script from 'next/script';
 import type { Metadata } from 'next';
 import '../styles/globals.scss';
 import Footer from '../components/footer/Footer';
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: 'Andris Rībens website',
   description:
     'This is Andris Rībens personal website, where I showcase some of my personal projects.',
-  // icons: { icon: '/favicon.ico', apple: '/apple-touch-icon.png' },
+  icons: { icon: '/favicon.ico', apple: '/apple-touch-icon.png' },
 };
 
 export default function RootLayout({
@@ -18,6 +18,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0J4LS936SG"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0J4LS936SG', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+      </head>
       <body>
         <main>{children}</main>
         <Footer />
